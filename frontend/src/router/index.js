747b1b2769802131be9196bbc30df1440f1b2e2c
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2025-05-04 15:21:14 +0800

    Menambahkan beberapa route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // Public
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView,
     },
     {
+      path: '/destinations',
+      name: 'Destinations',
+      component: AboutView,
+    },
+    {
+      path: '/destinations/:id',
+      name: 'DetailDestinations',
+      component: AboutView,
+      props: true,
+    },
+    {
+      path: '/suggest',
+      name: 'Suggest',
+      component: AboutView,
+    },
+    {
+      path: '/review',
+      name: 'Review',
+      component: AboutView,
+    },
+    {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      name: 'About',
+      component: AboutView,
+    },
+
+    // User Profile
+    {
+      path: '/profile',
+      component: AboutView,
+      children: [
+        { path: '', name: 'Profile', component: AboutView },
+        { path: 'wishlist', name: 'Wishlist', component: AboutView },
+        { path: 'reviews', name: 'UserReviews', component: AboutView },
+        { path: 'suggestions', name: 'UserSuggestions', component: AboutView },
+        { path: 'suggestions/:id', name: 'SuggestionDetail', component: AboutView },
+      ],
     },
   ],
 })
