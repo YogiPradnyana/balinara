b9e26bd43db79343374cc86a14264ac617a96cc9
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2025-05-16 23:10:19 +0800

    fix: memperbaiki responsivitas pada halaman home

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -7,10 +7,11 @@ import Leaf from '@/components/icons/Leaf.vue'
 import AI from '@/components/icons/AI.vue'
 import Send from '@/components/icons/Send.vue'
 import DoubleQuotes from '@/components/icons/DoubleQuotes.vue'
+import StarFiiled from '@/components/icons/StarFiiled.vue'
 </script>
 
 <template>
-  <div class="px-6 md:px-[140px]">
+  <div class="px-6 sm:px-16 lg:px-[140px]">
     <main class="flex-col flex items-center gap-8 mt-8 md:mt-16">
       <div class="flex flex-col gap-3 text-center items-center">
         <h1 class="text-[40px] md:text-[48px] font-semibold leading-14 md:leading-[62px] font-se">
@@ -55,131 +56,35 @@ import DoubleQuotes from '@/components/icons/DoubleQuotes.vue'
 
     <section class="mt-24 md:mt-30 flex-col flex gap-3">
       <div
-        class="px-4 text-pr-500 py-2 flex gap-2 w-fit items-center justify-center text-sm md:text-base font-medium outline-pr-500 outline rounded-full"
+        class="px-4 text-pr-500 py-2 flex gap-2 w-fit items-center justify-center text-sm sm:text-base font-medium outline-pr-500 outline rounded-full"
       >
         Destinations
       </div>
-      <div class="flex flex-col sm:flex-row sm:justify-between items-start">
+      <div class="flex flex-col lg:flex-row sm:justify-between items-start">
         <h1
           class="text-[32px] md:text-[48px] font-semibold leading-12 md:leading-[72px] max-w-[590px] font-se"
         >
           Find The Best <span class="text-pr-500">Destinations</span> in Bali
         </h1>
-        <div class="flex gap-2 md:gap-4 mt-3 items-center">
-          <p class="text-neu-600 text-sm md:text-base max-w-full sm:max-w-80 sm:text-end">
+        <div
+          class="flex gap-2 w-full justify-between lg:justify-end lg:w-fit md:gap-4 mt-3 items-center"
+        >
+          <p class="text-neu-600 text-sm md:text-base max-w-full lg:max-w-80 lg:text-end">
             Find your perfect Bali escape — explore beaches, temples, mountains, and local treasures
             effortlessly.
           </p>
-          <div class="p-4 flex items-center justify-center bg-pr-500 rounded-full">
-            <ArrowUpRight class="min-w-5 md:min-w-9 text-neu-50" />
+          <div class="p-4 flex items-center w-fit h-fit justify-center bg-pr-500 rounded-full">
+            <ArrowUpRight class="size-5 sm:size-9 text-neu-50" />
           </div>
         </div>
       </div>
       <div class="mt-8 whitespace-nowrap">
         <div class="flex gap-8 w-fit">
-          <div class="overflow-hidden w-72 md:w-[340px] h-96 md:h-[460px] rounded-4xl group">
-            <div
-              class="relative h-full w-full sm:group-hover:h-86 transition-all duration-500 ease-in-out"
-            >
-              <img
-                src="@/assets/images/ulun-danu-beratan.webp"
-                alt="Ubud Village"
-                class="object-cover w-full h-full rounded-4xl transition-transform duration-500 ease-in-out"
-              />
-              <div
-                class="flex flex-col justify-between absolute inset-0 items-end p-3 transition-opacity duration-500 ease-in-out"
-              >
-                <div class="p-2.5 flex items-center justify-center bg-sur-50 rounded-full">
-                  <Heart class="size-5 md:size-6 text-neu-900" />
-                </div>
-                <div class="flex justify-between items-end w-full">
-                  <div
-                    class="px-4 py-2.5 flex gap-1.5 items-center w-fit h-fit justify-center text-xs md:text-sm bg-sur-50 rounded-full text-neu-900 transition-all duration-500 ease-in-out"
-                  >
-                    Ulun Danu Beratan
-                    <ArrowUpRight class="size-4" />
-                  </div>
-                  <p class="text-[8px] md:text-[10px] text-neu-50">Photo by unsplash</p>
-                </div>
-              </div>
-              <div class="sm:static w-full px-2">
-                <h3 class="text-lg mt-4 leading-7 text-neu-900 font-semibold">Ulun Danu Beratan</h3>
-                <p class="text-neu-600 mt-1 line-clamp-2 whitespace-normal">
-                  Ulun Danu Beratan is a stunning Balinese Hindu temple located on Lake Beratan,
-                  appearing to float.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="overflow-hidden w-72 md:w-[340px] h-96 md:h-[460px] rounded-4xl group">
-            <div
-              class="relative h-full w-full sm:group-hover:h-86 transition-all duration-500 ease-in-out"
-            >
-              <img
-                src="@/assets/images/ulun-danu-beratan.webp"
-                alt="Ubud Village"
-                class="object-cover w-full h-full rounded-4xl transition-transform duration-500 ease-in-out"
-              />
-              <div
-                class="flex flex-col justify-between absolute inset-0 items-end p-3 transition-opacity duration-500 ease-in-out"
-              >
-                <div class="p-2.5 flex items-center justify-center bg-sur-50 rounded-full">
-                  <Heart class="size-5 md:size-6 text-neu-900" />
-                </div>
-                <div class="flex justify-between items-end w-full">
-                  <div
-                    class="px-4 py-2.5 flex gap-1.5 items-center w-fit h-fit justify-center text-xs md:text-sm bg-sur-50 rounded-full text-neu-900 transition-all duration-500 ease-in-out"
-                  >
-                    Ulun Danu Beratan
-                    <ArrowUpRight class="size-4" />
-                  </div>
-                  <p class="text-[8px] md:text-[10px] text-neu-50">Photo by unsplash</p>
-                </div>
-              </div>
-              <div class="sm:static w-full px-2">
-                <h3 class="text-lg mt-4 leading-7 text-neu-900 font-semibold">Ulun Danu Beratan</h3>
-                <p class="text-neu-600 mt-1 line-clamp-2 whitespace-normal">
-                  Ulun Danu Beratan is a stunning Balinese Hindu temple located on Lake Beratan,
-                  appearing to float.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="overflow-hidden w-72 md:w-[340px] h-96 md:h-[460px] rounded-4xl group">
-            <div
-              class="relative h-full w-full sm:group-hover:h-86 transition-all duration-500 ease-in-out"
-            >
-              <img
-                src="@/assets/images/ulun-danu-beratan.webp"
-                alt="Ubud Village"
-                class="object-cover w-full h-full rounded-4xl transition-transform duration-500 ease-in-out"
-              />
-              <div
-                class="flex flex-col justify-between absolute inset-0 items-end p-3 transition-opacity duration-500 ease-in-out"
-              >
-                <div class="p-2.5 flex items-center justify-center bg-sur-50 rounded-full">
-                  <Heart class="size-5 md:size-6 text-neu-900" />
-                </div>
-                <div class="flex justify-between items-end w-full">
-                  <div
-                    class="px-4 py-2.5 flex gap-1.5 items-center w-fit h-fit justify-center text-xs md:text-sm bg-sur-50 rounded-full text-neu-900 transition-all duration-500 ease-in-out"
-                  >
-                    Ulun Danu Beratan
-                    <ArrowUpRight class="size-4" />
-                  </div>
-                  <p class="text-[8px] md:text-[10px] text-neu-50">Photo by unsplash</p>
-                </div>
-              </div>
-              <div class="sm:static w-full px-2">
-                <h3 class="text-lg mt-4 leading-7 text-neu-900 font-semibold">Ulun Danu Beratan</h3>
-                <p class="text-neu-600 mt-1 line-clamp-2 whitespace-normal">
-                  Ulun Danu Beratan is a stunning Balinese Hindu temple located on Lake Beratan,
-                  appearing to float.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="overflow-hidden w-72 md:w-[340px] h-96 md:h-[460px] rounded-4xl group">
+          <div
+            v-for="i in 4"
+            :key="i"
+            class="overflow-hidden w-66 md:w-[340px] h-90 md:h-[460px] rounded-4xl group"
+          >
             <div
               class="relative h-full w-full sm:group-hover:h-86 transition-all duration-500 ease-in-out"
             >
@@ -261,7 +166,7 @@ import DoubleQuotes from '@/components/icons/DoubleQuotes.vue'
 
     <section class="mt-24 md:mt-30 flex-col flex items-center">
       <div
-        class="px-4 text-pr-500 py-2 flex gap-2 w-text-sm md:text-base items-center justify-center font-medium outline-pr-500 outline rounded-full"
+        class="px-4 text-pr-500 py-2 flex gap-2 text-sm sm:text-base items-center justify-center font-medium outline-pr-500 outline rounded-full"
       >
         Benefits
       </div>
@@ -335,7 +240,7 @@ import DoubleQuotes from '@/components/icons/DoubleQuotes.vue'
       <div class="flex flex-col xl:flex-row gap-8 items-center">
         <div class="w-full xl:w-5/12 flex-col items-start flex">
           <div
-            class="px-4 text-pr-500 py-2 text-sm md:text-base flex gap-2 w-fit items-center justify-center font-medium outline-pr-500 outline rounded-full"
+            class="px-4 text-pr-500 py-2 text-sm sm:text-base flex gap-2 w-fit items-center justify-center font-medium outline-pr-500 outline rounded-full"
           >
             Reviews
           </div>
@@ -395,11 +300,18 @@ import DoubleQuotes from '@/components/icons/DoubleQuotes.vue'
                     alt="User Profile"
                     class="size-10.5 rounded-full"
                   />
-                  <p
-                    class="hidden md:flex text-neu-900 font-medium whitespace-nowrap text-xs md:text-sm"
-                  >
-                    Udin Surudin
-                  </p>
+                  <div class="hidden md:flex flex-col gap-[2px]">
+                    <p class="flex text-neu-900 font-medium whitespace-nowrap text-xs md:text-sm">
+                      Udin Surudin
+                    </p>
+                    <div class="flex gap-[2px] items-center">
+                      <StarFiiled class="size-3.5" />
+                      <StarFiiled class="size-3.5" />
+                      <StarFiiled class="size-3.5" />
+                      <StarFiiled class="size-3.5" />
+                      <StarFiiled class="size-3.5" />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
